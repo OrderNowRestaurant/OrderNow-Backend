6d044185aa6a3106503ccd9a68b93326a8929ff8
VRLuis <luisvillarroldan1010@gmail.com>
2026-08-17 10:40:31 +0200

    feat: creating the request for creating a category

---
+++ src/main/java/ordernow/backend/ordernow_backend/requests/category/CreateCategoryRequest.java
@@ -0,0 +1,13 @@
+package ordernow.backend.ordernow_backend.requests.category;
+
+public class CreateCategoryRequest {
+    private String name;
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
