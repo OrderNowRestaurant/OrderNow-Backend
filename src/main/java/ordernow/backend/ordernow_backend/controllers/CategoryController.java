2c56f7a3e76033169b1b57fedbd92d9cee4986fc
VRLuis <luisvillarroldan1010@gmail.com>
2026-08-17 10:40:10 +0200

    feat: creating the create endpoint for the categories

--- src/main/java/ordernow/backend/ordernow_backend/controllers/CategoryController.java
+++ src/main/java/ordernow/backend/ordernow_backend/controllers/CategoryController.java
@@ -1,13 +1,12 @@
 package ordernow.backend.ordernow_backend.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import ordernow.backend.ordernow_backend.requests.dish.CreateDishRequest;
+import ordernow.backend.ordernow_backend.requests.category.CreateCategoryRequest;
 import ordernow.backend.ordernow_backend.responses.category.CategoryResponse;
 import ordernow.backend.ordernow_backend.services.CategoryService;
 
@@ -27,7 +26,11 @@ public class CategoryController {
     }
 
     @PostMapping("/create")
-    public CategoryResponse createCategory(/*@RequestBody CreateDishRequest createDishRequest*/) {
-        return this.categoryService.createCategory();
+    public CategoryResponse createCategory(@RequestBody CreateCategoryRequest createCategoryRequest) {
+        return this.categoryService.createCategory(createCategoryRequest);
     }
+
+    /*
+    @
+    */
 }
