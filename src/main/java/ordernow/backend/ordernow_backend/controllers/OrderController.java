fe8df2f6e9af17ad2763a0bb11eafe74babf43c3
VRLuis <luisvillarroldan1010@gmail.com>
2026-08-24 15:56:07 +0200

    perf: deleting the method of create

--- src/main/java/ordernow/backend/ordernow_backend/controllers/OrderController.java
+++ src/main/java/ordernow/backend/ordernow_backend/controllers/OrderController.java
@@ -1,12 +1,9 @@
 package ordernow.backend.ordernow_backend.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import ordernow.backend.ordernow_backend.requests.order.CreateNewOrderRequest;
 import ordernow.backend.ordernow_backend.responses.order.OrderResponse;
 import ordernow.backend.ordernow_backend.services.OrderService;
 
@@ -23,10 +20,5 @@ public class OrderController {
     @GetMapping("/get")
     public OrderResponse getAllOrders() {
         return this.orderService.getAllOrders();
-    }
-
-    @PostMapping("/create")
-    public OrderResponse createOrder(@RequestBody CreateNewOrderRequest createNewOrderRequest) {
-        return this.orderService.createOrder(createNewOrderRequest);
     }
 }
